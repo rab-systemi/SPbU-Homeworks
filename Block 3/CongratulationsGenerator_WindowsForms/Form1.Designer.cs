741383c8b794d0eac2dbebfb3e53fc2336e74bd1
Lev-Golofastov <l.golofastov@mail.ru>
2022-05-31 01:44:03 +0300

    Added WinForms version of task 2

---
+++ Block 3/CongratulationsGenerator_WindowsForms/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace CongratulationsGeneratorWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(35, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "День Рождения";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(182, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 63);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Новый Год";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(329, 35);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 63);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Рождество";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(476, 35);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 63);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "8 Марта";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(623, 35);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(141, 63);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "23 Февраля";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(182, 378);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(435, 51);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Сгенерировать";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(35, 131);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(729, 172);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(639, 309);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(125, 43);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Копировать";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.Text = "Генератор поздравлений";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private TextBox textBox1;
+        private Button button7;
+    }
+}
---
+++ Block 3/CongratulationsGenerator_WindowsForms/Form1.cs
@@ -0,0 +1,94 @@
+namespace CongratulationsGeneratorWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string holiday = default;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            holiday = "birthday";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            holiday = "new-year";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            holiday = "christmas";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            holiday = "woman";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            holiday = "man";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string url = $"https://pozdrav.in/gen/{holiday}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = client.GetAsync(url).Result)
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string result = content.ReadAsStringAsync().Result;
+                        int i = 0;
+                        string message = default;
+
+                        if (holiday == "man" | holiday == "woman")
+                        {
+                            foreach (var ch in result)
+                            {
+                                if (i == 119)
+                                {
+                                    message = message + ch;
+                                }
+                                if (ch == '\n')
+                                {
+                                    i++;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            foreach (var ch in result)
+                            {
+                                if (i == 130)
+                                {
+                                    message = message + ch;
+                                }
+                                if (ch == '\n')
+                                {
+                                    i++;
+                                }
+                            }
+                        }
+                        message = (message.Remove(0, 75)).Trim();
+                        char[] MyChar = { '<', '/', 's', 'p', 'a', 'n', '>' };
+                        message = (message.TrimEnd(MyChar)).Trim();
+                        
+                        textBox1.Text = message;
+                    }
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+    }
+}
